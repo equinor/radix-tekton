1fe8b5c0d4152c0a2697fe28d95f710267299561
Sergey Smolnikov <ssmol@equinor.com>
2022-12-05 13:40:18 +0100

    Corrected detection of changed folders (#50)
    * Corrected detection of changed folders
    
    * Cleanup code

--- pkg/pipeline/prepare_pipelines.go
+++ pkg/pipeline/prepare_pipelines.go
@@ -162,18 +162,28 @@ func componentHasChangedSource(envName string, component v1.RadixCommonComponent
 		return false
 	}
 
-	sourceFolder := commonUtils.TernaryString(len(component.GetSourceFolder()) == 0, ".", component.GetSourceFolder())
-	sourceFolderWithTrailingSlash := fmt.Sprintf("%s/", path.Dir(fmt.Sprintf("%s/", sourceFolder)))
-	if path.Dir(sourceFolderWithTrailingSlash) == path.Dir(".") && len(changedFolders) > 0 {
+	sourceFolder := cleanPathAndSurroundBySlashes(component.GetSourceFolder())
+	if path.Dir(sourceFolder) == path.Dir("/") && len(changedFolders) > 0 {
 		return true //for components with the repository root as a 'src' - changes in any repository sub-folders are considered also as the component changes
 	}
 
 	for _, folder := range changedFolders {
-		if strings.HasPrefix(folder, sourceFolderWithTrailingSlash) {
+		if strings.HasPrefix(cleanPathAndSurroundBySlashes(folder), sourceFolder) {
 			return true
 		}
 	}
 	return false
+}
+
+func cleanPathAndSurroundBySlashes(dir string) string {
+	if !strings.HasSuffix(dir, "/") {
+		dir = fmt.Sprintf("%s/", dir)
+	}
+	dir = fmt.Sprintf("%s/", path.Dir(dir))
+	if !strings.HasPrefix(dir, "/") {
+		return fmt.Sprintf("/%s", dir)
+	}
+	return dir
 }
 
 func (ctx *pipelineContext) preparePipelinesJobForTargetEnv(namespace, envName, timestamp string) (bool, string, error) {
