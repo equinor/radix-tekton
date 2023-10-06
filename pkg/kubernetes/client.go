e9c93e7711bd5bfcaacbc66ee0a58e99fbdf9b34
Nils Gustav Stråbø <nst@equinor.com>
2023-10-06 15:49:54 +0200

    k8s client: try connect with retry on tls handshake timeout

--- pkg/kubernetes/client.go
+++ pkg/kubernetes/client.go
@@ -1,11 +1,15 @@
 package kubernetes
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
 	radixclient "github.com/equinor/radix-operator/pkg/client/clientset/versioned"
+	log "github.com/sirupsen/logrus"
 	tektonclient "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -15,6 +19,7 @@ import (
 func GetClients() (kubernetes.Interface, radixclient.Interface, tektonclient.Interface, error) {
 	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	ctx := context.Background()
 
 	if err != nil {
 		config, err = rest.InClusterConfig()
@@ -23,20 +28,53 @@ func GetClients() (kubernetes.Interface, radixclient.Interface, tektonclient.Int
 		}
 	}
 
-	kubeClient, err := kubernetes.NewForConfig(config)
+	kubeClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*kubernetes.Clientset, error) {
+		return kubernetes.NewForConfig(config)
+	})
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("getClusterConfig k8s kubeClient: %v", err)
+		return nil, nil, nil, fmt.Errorf("failed to create Kubernetes resource client: %w", err)
 	}
 
-	radixClient, err := radixclient.NewForConfig(config)
+	radixClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*radixclient.Clientset, error) {
+		return radixclient.NewForConfig(config)
+	})
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("getClusterConfig radix kubeClient: %v", err)
+		return nil, nil, nil, fmt.Errorf("failed to create Radix resource client: %w", err)
 	}
 
-	tektonClient, err := tektonclient.NewForConfig(config)
+	tektonClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*tektonclient.Clientset, error) {
+		return tektonclient.NewForConfig(config)
+	})
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("getClusterConfig radix tektonClient: %v", err)
+		return nil, nil, nil, fmt.Errorf("failed to create Tekton resource client: %w", err)
 	}
 
 	return kubeClient, radixClient, tektonClient, err
+}
+
+func waitForClientWithSuccessfulConnection[T interface{ RESTClient() rest.Interface }](ctx context.Context, clientFactory func() (T, error)) (T, error) {
+	var client T
+	timeoutCtx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	err := wait.PollUntilContextCancel(timeoutCtx, 15*time.Second, true, func(ctx context.Context) (done bool, err error) {
+		c, err := clientFactory()
+		if err != nil {
+			return false, err
+		}
+
+		// Retry if error transient, e.g. TLS handshake timeout
+		if err := c.RESTClient().Get().Do(timeoutCtx).Error(); err != nil && isTransientConnectionError(err) {
+			log.Infof("transient error when connecting, retrying: %v", err)
+			return false, nil
+		}
+		client = c
+		return true, nil
+	})
+	return client, err
+}
+
+func isTransientConnectionError(err error) bool {
+	// TODO: Should we check for other connection errors that are transient, e.g. net.DNSError?
+	return err.Error() == "net/http: TLS handshake timeout"
 }
