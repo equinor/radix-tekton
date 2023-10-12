9f7ee2c78050afed43172688b569aac0b021e1f7
Nils Gustav Stråbø <nst@equinor.com>
2023-10-12 09:24:17 +0200

    update module version

--- pkg/kubernetes/client.go
+++ pkg/kubernetes/client.go
@@ -6,10 +6,9 @@ import (
 	"os"
 	"time"
 
+	operatorutils "github.com/equinor/radix-operator/pkg/apis/utils"
 	radixclient "github.com/equinor/radix-operator/pkg/client/clientset/versioned"
-	log "github.com/sirupsen/logrus"
 	tektonclient "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -28,21 +27,21 @@ func GetClients() (kubernetes.Interface, radixclient.Interface, tektonclient.Int
 		}
 	}
 
-	kubeClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*kubernetes.Clientset, error) {
+	kubeClient, err := operatorutils.PollUntilRESTClientSuccessfulConnection(ctx, time.Minute, 15*time.Second, func() (*kubernetes.Clientset, error) {
 		return kubernetes.NewForConfig(config)
 	})
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("failed to create Kubernetes resource client: %w", err)
 	}
 
-	radixClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*radixclient.Clientset, error) {
+	radixClient, err := operatorutils.PollUntilRESTClientSuccessfulConnection(ctx, time.Minute, 15*time.Second, func() (*radixclient.Clientset, error) {
 		return radixclient.NewForConfig(config)
 	})
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("failed to create Radix resource client: %w", err)
 	}
 
-	tektonClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*tektonclient.Clientset, error) {
+	tektonClient, err := operatorutils.PollUntilRESTClientSuccessfulConnection(ctx, time.Minute, 15*time.Second, func() (*tektonclient.Clientset, error) {
 		return tektonclient.NewForConfig(config)
 	})
 	if err != nil {
@@ -50,31 +49,4 @@ func GetClients() (kubernetes.Interface, radixclient.Interface, tektonclient.Int
 	}
 
 	return kubeClient, radixClient, tektonClient, err
-}
-
-func waitForClientWithSuccessfulConnection[T interface{ RESTClient() rest.Interface }](ctx context.Context, clientFactory func() (T, error)) (T, error) {
-	var client T
-	timeoutCtx, cancel := context.WithTimeout(ctx, 60*time.Second)
-	defer cancel()
-
-	err := wait.PollUntilContextCancel(timeoutCtx, 15*time.Second, true, func(ctx context.Context) (done bool, err error) {
-		c, err := clientFactory()
-		if err != nil {
-			return false, err
-		}
-
-		// Retry if error transient, e.g. TLS handshake timeout
-		if err := c.RESTClient().Get().Do(timeoutCtx).Error(); err != nil && isTransientConnectionError(err) {
-			log.Infof("transient error when connecting, retrying: %v", err)
-			return false, nil
-		}
-		client = c
-		return true, nil
-	})
-	return client, err
-}
-
-func isTransientConnectionError(err error) bool {
-	// TODO: Should we check for other connection errors that are transient, e.g. net.DNSError?
-	return err.Error() == "net/http: TLS handshake timeout"
 }
