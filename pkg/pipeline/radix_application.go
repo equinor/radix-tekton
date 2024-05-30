143582b4f16e0cab0634eb09667fb62e98ddc72b
Nils Gustav Stråbø <nst@equinor.com>
2024-05-30 16:07:52 +0200

    update radix-operator version

--- pkg/pipeline/radix_application.go
+++ pkg/pipeline/radix_application.go
@@ -1,13 +1,15 @@
 package pipeline
 
 import (
+	"context"
+
 	"github.com/equinor/radix-operator/pipeline-runner/steps"
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	"github.com/equinor/radix-tekton/pkg/utils/configmap"
 )
 
 func (ctx *pipelineContext) createRadixApplicationFromContent(configFileContent string) (*v1.RadixApplication, error) {
-	return steps.CreateRadixApplication(ctx.radixClient, ctx.env.GetDNSConfig(), configFileContent)
+	return steps.CreateRadixApplication(context.TODO(), ctx.radixClient, ctx.env.GetDNSConfig(), configFileContent)
 }
 
 func (ctx *pipelineContext) createRadixApplicationFromConfigMap() (*v1.RadixApplication, error) {
