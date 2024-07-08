ac7870de9d0cf94702afa14a7d5e999e1fbdcee5
Sergey Smolnikov <ssmol@equinor.com>
2024-07-08 16:19:52 +0200

    Ignore get commit-id error for promote pipeline

--- pkg/pipeline/prepare_pipelines.go
+++ pkg/pipeline/prepare_pipelines.go
@@ -48,6 +48,10 @@ func (ctx *pipelineContext) preparePipelinesJob() (*model.PrepareBuildContext, e
 
 	err = git.ResetGitHead(ctx.env.GetGitRepositoryWorkspace(), gitHash)
 	if err != nil {
+		if ctx.env.GetRadixPipelineType() == v1.Promote {
+			log.Error().Msgf("Failed to find Git CommitID %s. Ignore the error for the Promote pipeline: %v", gitHash, err)
+			return &buildContext, nil
+		}
 		return nil, err
 	}
 
