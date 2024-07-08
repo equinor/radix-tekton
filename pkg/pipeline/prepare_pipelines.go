8eed962dfae8efb515422c59db5ed9acac39455b
Sergey Smolnikov <ssmol@equinor.com>
2024-07-08 16:37:46 +0200

    Troubleshoot not found commit on Promote pipeline (#137)
    * Ignore get commit-id error for promote pipeline
    
    * Corrected error message

--- pkg/pipeline/prepare_pipelines.go
+++ pkg/pipeline/prepare_pipelines.go
@@ -48,6 +48,10 @@ func (ctx *pipelineContext) preparePipelinesJob() (*model.PrepareBuildContext, e
 
 	err = git.ResetGitHead(ctx.env.GetGitRepositoryWorkspace(), gitHash)
 	if err != nil {
+		if ctx.env.GetRadixPipelineType() == v1.Promote {
+			log.Error().Msgf("Failed to find Git CommitID %s. Error: %v. Ignore the error for the Promote pipeline. If Sub-pipeline exists it is skipped.", gitHash, err)
+			return &buildContext, nil
+		}
 		return nil, err
 	}
 
